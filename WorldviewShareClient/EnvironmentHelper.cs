a79de53138bfefd710f27a73a4366934463abd6e
Jupe <55913455+JupeOwl@users.noreply.github.com>
2024-11-08 21:27:29 +0200

    Add base for user id generation and fetching

---
+++ WorldviewShareClient/EnvironmentHelper.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using WorldviewShareClient.Models;
+
+namespace WorldviewShareClient;
+
+public static class EnvironmentHelper
+{
+    private static EnvironmentSettings _settings;
+
+    public static EnvironmentSettings GetEnvironment()
+    {
+        if (_settings == null)
+        {
+            if (!File.Exists("environment.env"))
+            {
+                File.Copy("environment.env.default", "environment.env");
+
+                _settings = new EnvironmentSettings
+                {
+                    Id = Guid.NewGuid()
+                };
+
+                using var writer = new StreamWriter("environment.env");
+                var json = JsonSerializer.Serialize(_settings);
+                writer.Write(json);
+            }
+            else
+            {
+                using var reader = new StreamReader("environment.env");
+                var json = reader.ReadToEnd();
+                _settings = JsonSerializer.Deserialize<EnvironmentSettings>(json);
+            }
+        }
+
+        return _settings;
+    }
+}
