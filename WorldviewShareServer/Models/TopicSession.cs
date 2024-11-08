860a0795bc57a1ee1c727db615e66f537a2b3f95
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-11-08 23:13:03 +0200

    Change ICollection to List

--- WorldviewShareServer/Models/TopicSession.cs
+++ WorldviewShareServer/Models/TopicSession.cs
@@ -5,5 +5,5 @@ public class TopicSession
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Topic { get; set; }
-    public ICollection<User> Users { get; set; }
+    public List<User> Users { get; set; }
 }
