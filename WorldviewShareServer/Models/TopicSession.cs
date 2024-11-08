3eb186b2e35057fd2a736984c87dbb70c5a0093b
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-11-09 01:21:34 +0200

    Add default values to model types

--- WorldviewShareServer/Models/TopicSession.cs
+++ WorldviewShareServer/Models/TopicSession.cs
@@ -3,7 +3,7 @@ namespace WorldviewShareServer.Models;
 public class TopicSession
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Topic { get; set; }
-    public List<User> Users { get; set; }
+    public string Name { get; set; } = "";
+    public string Topic { get; set; } = "";
+    public List<User> Users { get; set; } = [];
 }
--- WorldviewShareServer/Models/User.cs
+++ WorldviewShareServer/Models/User.cs
@@ -3,6 +3,6 @@ namespace WorldviewShareServer.Models;
 public class User
 {
     public Guid Id { get; set; }
-    public string Username { get; set; }
-    public List<TopicSession> TopicSessions { get; set; }
+    public string Username { get; set; } = "";
+    public List<TopicSession> TopicSessions { get; set; } = [];
 }
