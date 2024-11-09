046e3da9075f61b0f95eb2be862ee663a96808b5
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-11-09 12:14:54 +0200

    Remove unused usings

--- WorldviewShareServer/Data/WorldviewShareContext.cs
+++ WorldviewShareServer/Data/WorldviewShareContext.cs
@@ -1,7 +1,4 @@
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 using WorldviewShareServer.Models;
 namespace WorldviewShareServer.Data;
 
