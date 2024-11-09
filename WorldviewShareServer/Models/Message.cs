5acc61ab7c652feade20422d4feef54308004747
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-11-09 19:28:28 +0200

    Add max length constraint on server side

--- WorldviewShareServer/Models/Message.cs
+++ WorldviewShareServer/Models/Message.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 namespace WorldviewShareServer.Models;
 
 public class Message
 {
     public Guid Id { get; set; }
+    [MaxLength(256)]
     public string Content { get; set; } = "";
     public User Author { get; set; } = null!;
     public Guid AuthorId { get; set; }
