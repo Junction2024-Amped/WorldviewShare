1f37300eb0b625994659894a723dcd73ad9edb0a
Jupe <55913455+JupeOwl@users.noreply.github.com>
2024-11-09 19:57:54 +0200

    Base for Sources

--- WorldviewShareClient/ViewModels/MessageViewModel.cs
+++ WorldviewShareClient/ViewModels/MessageViewModel.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Windows.Input;
+
 namespace WorldviewShareClient.ViewModels;
 
 public class MessageViewModel : ViewModelBase
 {
     private string _message;
     private string _name;
+    private bool _showSource;
+    private Uri? _source;
 
     public string Message
     {
@@ -12,6 +17,30 @@ public class MessageViewModel : ViewModelBase
         {
             if (value == _message) return;
             _message = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand HandleLinkCommand { get; }
+
+    public bool ShowSource
+    {
+        get => _showSource;
+        set
+        {
+            if (value == _showSource) return;
+            _showSource = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Uri? Source
+    {
+        get => _source;
+        set
+        {
+            if (Equals(value, _source)) return;
+            _source = value;
             OnPropertyChanged();
         }
     }
